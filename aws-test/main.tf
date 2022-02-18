ea81619c2a2e25e35f8c3d194333952ce75a1a50
Bjorn Boe <bvboe@pobox.com>
2022-02-18 15:44:11 -0500

    Initial version

---
+++ aws-test/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-01b996646377b6619"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.us-east-1a-public.id}"
+}
+
+resource "aws_vpc" "example" {
+  tags = {
+    Name = "main"
+  }
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
+
+resource "aws_subnet" "us-east-1a-public" {
+  vpc_id = "${aws_vpc.example.id}"
+  cidr_block = "10.0.1.0/25"
+  availability_zone = "us-east-1a"
+}
